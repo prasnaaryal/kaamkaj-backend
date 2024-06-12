7c0ce251b22ec5f6020b1366423de2948b9a48b6
Prasna Aryal <prasna101@gmail.com>
2024-06-12 16:43:46 +0545

    user routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 // Import the routes from other files
 import authRoutes from "./auth/authRoutes.js";
+import userRoutes from "./user/userRoutes.js";
 import jobRoutes from "./job/jobRoutes.js";
 import categoryRoutes from "./category/categoryRoutes.js";
 
@@ -10,5 +11,7 @@ const router = express.Router();
 router.use("/auth", authRoutes); // localhost:9000/api/auth/register or localhost:9000/api/auth/login
 router.use("/job", jobRoutes); // localhost:9000/api/job
 router.use("/category", categoryRoutes); // localhost:9000/api/category
+router.use("/user", userRoutes); // localhost:9000/api/user/load-user
+
 
 export default router;
